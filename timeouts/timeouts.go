ee9adb1cefd29e7decf3d02c5c5c7f54627e423c
Paul Soberanes <paulsoberanes@gmail.com>
2018-08-01 11:21:09 -0500

    Timeouts example

---
+++ timeouts/timeouts.go
@@ -0,0 +1,38 @@
+package main
+
+import "time"
+import "fmt"
+
+// Timeouts are important fo rprograms that connect to external
+// resources or that otherwise need to bound execution time.
+// Implementing timeouts in Go is easy and elegant thanks to
+// channels and select.
+
+func main() {
+
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
+	}
+
+}
